71c90c1c3b0f7a48dc9b264d7f4888739efc61e8
nickKengne <nickdk294@gmail.com>
2024-06-27 11:26:48 +0100

    modify some sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-
 import { CloudCircle } from "@/components/CloudCircle";
 import { Verified } from "@/components/magicui/tweet-card";
 import { TweetCard } from "@/components/magicui/TweetCard";
@@ -13,27 +11,42 @@ import React from "react";
 import Banner from "./components/Banner";
 
 export default function page() {
+
+  const post = [
+    {
+      id: "1779784360448979052"
+    }, {
+      id: "1803115094588449022"
+    },{
+      id: "1762842753610776751"
+    }
+  ]
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center p-7">
       <ThemeToggle className="fixed z-50 right-5 top-5" />
       <NavBar />
-      <Banner/>
+      <Banner />
       <div className="h-auto max-w-[100%] relative w-[100%]">
-        {/* <p className="text-center text-7xl font-bold"></p> */}
-      <ParallaxPortfolio/>
+        <ParallaxPortfolio />
       </div>
-      <MarqueeCard/>
-      <div className="flex gap-2 flex-wrap justify-center items-center">
-        <TweetCard id="1762842753610776751" />
-        <TweetCard id="1779784360448979052" />
-        <TweetCard id="1803115094588449022" />
-      </div>
-      {/* <PostLinkdin/> */}
-
       
+      <h1 className="text-primary font-semibold leading-7">
+        LinkedIn
+      </h1>
+      <p className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">
+        Posts LinkedIn that's interested you !
+      </p>
 
+      <div className="flex gap-2 mt-8 flex-wrap justify-center items-center">
+        {
+          post.map((post) => (
+            <TweetCard id={post.id} key={post.id}/>
+          ))
+        }
+      </div>
+      <MarqueeCard />
+      {/* <PostLinkdin/> */}
     </div>
   );
 }
-
-
--- components/hero-parallax.tsx
+++ components/hero-parallax.tsx
@@ -57,7 +57,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[300vh] py-10 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
@@ -105,12 +105,10 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> Portfolio studio
+        Latest works<br /> Portfolio studio
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+      For almost 3 years I have shared a pretty crazy interest in programming, through my different projects, you will see what I can produce!
       </p>
     </div>
   );
